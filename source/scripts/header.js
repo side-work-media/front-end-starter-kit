d100fa8793c228600fcb6539b0cc3f0cdf7966c3
Christopher Dierkens <cjdierkens@gmail.com>
2016-02-21 22:01:07 -0500

    Added a module example, jquery, styles, and more.

---
+++ source/scripts/header.js
@@ -0,0 +1,43 @@
+var $ = require("jQuery");
+
+var $wrapper,
+    $nav,
+    height,
+    scrollTop;
+
+function hide () {
+    $nav.css("top", -height + "px");
+}
+
+function show () {
+    $nav.css({
+        position: "fixed",
+        top: 0
+    });
+
+    $wrapper.css("padding-top", 0);
+}
+
+function onScroll () {
+    if (scrollTop <= height) {
+        show();
+    } else if (scrollTop > $(window).scrollTop()){
+        show();
+    } else {
+        hide();
+    }
+
+    scrollTop = $(window).scrollTop();
+}
+
+function init () {
+    $wrapper = $("header");
+    $nav = $wrapper.find("nav");
+    height = $wrapper.outerHeight();
+
+    $wrapper.css("height", height + "px");
+
+    $(window).on("scroll", onScroll);
+}
+
+module.exports.init = init;
--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,18 +1,3 @@
-var _ = require("lodash");
+var header = require("./header");
 
-// Fetch the name of the first pet from each owner
-var ownerArr = [{
-    "owner": "Colin",
-    "pets": [{"name":"dog1"}, {"name": "dog2"}]
-}, {
-    "owner": "John",
-    "pets": [{"name":"dog3"}, {"name": "dog4"}]
-}];
-
-// Array's map method.
-ownerArr.map(function(owner){
-   return owner.pets[0].name;
-});
-
-// Lodash
-console.log(_.map(ownerArr, 'pets[0].name'));
+header.init();
